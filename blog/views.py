d9c6f45fb61a7ea3a62e2cc49f7b3d65b50ddf9c
firasbarkia <firasbarkia090@gmail.com>
2024-12-19 17:47:27 +0100

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,4 @@ from .models import Post
 from django.utils.timezone import now
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'templates/blog/post_list.html', {})
